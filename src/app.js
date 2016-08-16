be61f9986b5421b4138455ae19d2cd995022f33d
Rob Bos <admin@stuiterveer.com>
2016-08-16 23:18:19 +0200

    Improved source readability
    Added comments and changed some lines a bit to make things a bit more grouped together and readable.

--- src/app.js
+++ src/app.js
@@ -11,30 +11,31 @@ var directions = {
   'up':'',
   'down':''
 };
+
+//Configure the main view
 var card = new UI.Card({
   body: '                       UP ->\n\n     LEFT/RIGHT ->\n\n\n                 DOWN ->'
 });
-
 // Disable the action bar
 card.action(false);
+//Display main view
 card.show();
 
+//Register handlers for all four directions
 card.on('click', 'up', function() {
   controlCamera('up');
 });
-
 card.on('click', 'down', function() {
   controlCamera('down');
 });
-
 card.on('click', 'select', function() {
   controlCamera('left');
 });
-
 card.on('longClick', 'select', function() {
   controlCamera('right');
 });
 
+//Control the camera through GET request
 function controlCamera(button){
   ajax({
     url: protocol + '://' + username + ':' + password + '@' + URL + directions[button],

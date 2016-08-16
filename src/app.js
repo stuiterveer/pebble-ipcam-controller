911b0f67668dbb19017820fd07d2d8d67728548b
Rob Bos <admin@stuiterveer.com>
2016-08-17 01:03:57 +0200

    Add configuration page
    Traded in the placeholder variables for a configuration page so all needed data can be set through the Pebble app on the phone.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,11 @@
 var UI = require('ui');
 var ajax = require('ajax');
-var protocol = ''; //Insert connection protocol (HTTP/HTTPS) here
-var username = ''; //Insert username here
-var password = ''; //Insert password here
-var URL = ''; //Insert control URL for the directions here, which should also include all GET data that should be sent by default
-var directions = {
-  //Insert all GET data here that is specific for the directions
-  'left':'',
-  'right':'',
-  'up':'',
-  'down':''
-};
+var Settings = require('settings');
+
+//Define configuration page
+Settings.config({
+  url: 'https://stuiterveer.com/pebbleconfig/202143fc-2f98-4f6b-87b8-af6ee27eea7b/index.html'
+});
 
 //Configure the main view
 var card = new UI.Card({
@@ -38,6 +33,6 @@ card.on('longClick', 'select', function() {
 //Control the camera through GET request
 function controlCamera(button){
   ajax({
-    url: protocol + '://' + username + ':' + password + '@' + URL + directions[button],
+    url: Settings.option('protocol') + '://' + Settings.option('username') + ':' + Settings.option('password') + '@' + Settings.option('URL') + Settings.option(button)
   });
 }
